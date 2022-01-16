5c3e0dd5d66bf840350aa3e4e8e554e583acf584
BHAVYA TYAGI <72131872+Bhavya1912@users.noreply.github.com>
2022-01-16 22:02:35 +0530

    Create java2.java

---
+++ java2.java
@@ -0,0 +1,65 @@
+package com.bhavya;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter name");
+        String name = sc.nextLine();
+        System.out.print("your name is ");
+        System.out.println(name);
+
+
+        Scanner scc = new Scanner(System.in);
+        System.out.println("enter age");
+        int age = sc.nextInt();
+        if (age >= 18)
+            System.out.println("you can vote");
+        else
+            System.out.println("you can not vote");
+
+
+        Scanner sccc = new Scanner(System.in);
+        System.out.println("time");
+
+
+        boolean isSunUp = false;
+        { if (isSunUp == true)
+            System.out.println("day");
+        else
+            System.out.println("night");
+        }
+         int a=40;
+         int b= 2;
+
+         if (a>20||b>40);
+         System.out.println("true statements");
+
+         Scanner Scs = new Scanner(System.in);
+
+         int cash = sc.nextInt();
+         if (cash<10){
+             System.out.println("can not buy anything");
+             System.out.println("get more cash");
+         }
+         else if (cash>10 && cash<50);
+        {
+            System.out.println("can buy pen");
+        }
+        
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
